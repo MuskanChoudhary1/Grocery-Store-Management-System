a99011f1fe9192c6942ae81729d0d7d5b9a6be95
Muskan Choudhary <159620237+MuskanChoudhary1@users.noreply.github.com>
2025-05-08 14:02:03 +0530

    Add files via upload

---
+++ backend/sql_connection.py
@@ -0,0 +1,18 @@
+import datetime
+import mysql.connector
+
+
+__cnx = None
+def get_sql_connection():
+    print("Opening mysql connection")
+    global __cnx
+    if __cnx is None:
+        __cnx = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password="root",
+            database="mqsql"
+            )
+        
+    return __cnx
+
